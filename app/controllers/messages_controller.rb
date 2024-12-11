87e5a559d3ce61d4acde9e606df6a46d9d2d7c96
ham86-noob <new3298mini-taku@outlook.jp>
2024-12-11 19:18:54 +0900

    簡単な修正。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,14 @@ class MessagesController < ApplicationController
     def create
         if current_user.entries.where(room_id: params[:message][:room_id]).present?
             @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(user_id: current_user.id))
-            reciever = Entry.where(room_id: params[:message][:room_id]).where.not(user_id: current_user.id).first.user
-            Notification.create(recipient_id: reciever.id, actor_id: current_user.id, notifiable: @message.room, action: "メッセージ送信")
-            reciever.update(notifications_count: reciever.notifications_count + 1)
+            entries = Entry.where(room_id: params[:message][:room_id])
+
+            entries.each do |e|
+                if e.user.id != current_user.id
+                    Notification.create(recipient_id: e.user.id, actor_id: current_user.id, notifiable: @message.room, action: "メッセージ送信")
+                    e.user.update(notifications_count: e.user.notifications_count + 1)
+                end
+            end
         else
             flash[:alert] = "メッセージ送信に失敗しました。"
         end
